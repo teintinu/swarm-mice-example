a6603b0e0dceeca9129bff66dc7382f0391012b7
olebedev <oolebedev@gmail.com>
2015-12-29 09:52:10 +0500

    Handle mouse click

--- components/Mouse.js
+++ components/Mouse.js
@@ -4,11 +4,23 @@ import { mouse } from './mice.css'
 
 class Mouse extends Component {
 
+  handleClick() {
+    this.props.set({
+      symbol: String.fromCharCode(10000 + Math.round(Math.random() * 10000 % 60))
+    })
+  }
+
   render() {
-    return <span className={mouse} style={{
-      top: this.props.data.y + 'px',
-      left: this.props.data.x + 'px'
-    }}>{this.props.data.symbol}</span>
+    return <span
+      onClick={this.handleClick.bind(this)}
+      className={mouse}
+      style={{
+        top: this.props.data.y + 'px',
+        left: this.props.data.x + 'px'
+      }}
+    >
+      {this.props.data.symbol}
+    </span>
   }
 
 }
