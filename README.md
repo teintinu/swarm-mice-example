625597838d99d67e08dbd743ef0d336a407aa28a
olebedev <oolebedev@gmail.com>
2015-12-30 11:52:14 +0500

    Add production build

--- README.md
+++ README.md
@@ -5,6 +5,13 @@
 $ npm i
 $ npm run server
 $ npm start # in other terminal process
+$ open http://localhost:3000
+```
+
+## Static build
+```bash
+$ NODE_ENV=production webpack
+$ open ./index.html
 ```
 
 ## License
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,23 @@ var path = require('path')
 var webpack = require('webpack')
 
 var cssLoader = 'style-loader!css-loader?modules'
-if (process.env.NODE_ENV !== 'production') {
+
+var plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.NoErrorsPlugin()
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins = plugins.concat([
+    new webpack.optimize.UglifyJsPlugin({
+      output: {comments: false}
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: JSON.stringify('production')}
+    })
+  ]);
+} else {
   cssLoader += '&localIdentName=[name]__[local]___[hash:base64:5]'
 }
 
@@ -13,11 +29,7 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
+  plugins: plugins,
   module: {
     loaders: [
       { test: /\.css$/, loader: cssLoader },
